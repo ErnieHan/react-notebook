574d6dd9bb6c1e1be53be282ead3cb9aad66cb93
Ernie <ernie.yeh.t.h@emphasis.com.tw>
2020-02-06 17:32:54 +0800

    ernie 01
    enrie 02

--- __tests__/pass.test.js
+++ __tests__/pass.test.js
@@ -1,19 +1,33 @@
 import React from "react";
 import { shallow } from "enzyme";
-import IsPass from "../src/components/IsPass/index";
-// import { Content } from "../src/components/IsPass/index";
+import IsPass from "../src/components/IsPass";
 
-describe("newsTop元件測試", () => {
-  describe("newsTop內容測試", () => {
-    let component;
-    //每次測試前都執行渲染元件
-    beforeEach(() => {
-      component = shallow(<IsPass />);
-      console.log(component.debug());
-    });
+describe("TESTING_ISPASS_COMPONENT", () => {
+  const component = shallow(<IsPass />);
 
-    it("NewsTop元件是否有顯示", () => {
-      expect(component.find("Content")).toHaveLength(1);
-    });
+  test("COMPONENT_DISPLAY_SUCCESSFUL", () => {
+    expect(component.find("Content")).toHaveLength(1);
+    expect(component.find("Content").text()).toEqual("foo");
+  });
+
+  test("CLICK_TEST", () => {
+    const handleClick = jest.spyOn(component.instance(), "handleClick");
+    component.instance().handleClick();
+    expect(handleClick).toBeCalled();
+    expect(component.find("Content").text()).toEqual("bar");
+  });
+
+  test("ONCHANGE_TEST", () => {
+    component.find("Input").simulate("change", { target: { value: "ernie" } });
+    expect(component.find("Input").prop("value")).toEqual("ernie");
+  });
+
+  test("FUNCTION_CALLBACK", () => {
+    const instance = component.instance();
+    const login = jest.spyOn(instance, "login");
+    instance.forceUpdate();
+    component.find("div.ernie").simulate("click");
+    expect(login).toHaveBeenCalledTimes(3);
+    console.log(instance);
   });
 });
--- src/components/IsPass/index.js
+++ src/components/IsPass/index.js
@@ -5,11 +5,45 @@ export const Content = styled.div`
   padding: 15px;
 `;
 
-export class IsPass extends Component {
+export const Input = styled.input`
+  padding: 15px;
+`;
+
+class IsPass extends Component {
+  state = {
+    name: "foo"
+  };
+
+  handleClick = () => {
+    this.setState({
+      name: "bar"
+    });
+  };
+
+  handleChange = e => {
+    this.setState({
+      name: e.target.value
+    });
+  };
+
+  login() {
+    console.log("aaa");
+  }
+
   render() {
     return (
       <div>
-        <Content className="testing">123</Content>
+        <Content className="testing" onClick={this.handleClick}>
+          {this.state.name}
+        </Content>
+        <Input
+          type="text"
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <div className="ernie" onClick={this.login.bind(this)}>
+          Testing
+        </div>
       </div>
     );
   }
